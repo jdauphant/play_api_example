00bc362f6c36e5564caa53e64815865af744f03a
Julien DAUPHANT <judauphant@gmail.com>
2014-12-10 11:59:44 +0100

    Correct typo

--- app/Global.scala
+++ app/Global.scala
@@ -8,18 +8,18 @@ import scala.concurrent.Future
 
 object Global extends GlobalSettings {
 
-  def toErrorStringToJson(error: String) = Json.toJson(TopLevel(errors=Some(Error(error))))
+  def errorStringToJson(error: String) = Json.toJson(TopLevel(errors=Some(Error(error))))
 
   override def onBadRequest(request: RequestHeader, error: String) = {
-    Future.successful(BadRequest(toErrorStringToJson(error)).as("application/vnd.api+json"))
+    Future.successful(BadRequest(errorStringToJson(error)).as("application/vnd.api+json"))
   } 
 
   override def onError(request: RequestHeader, throwable: Throwable) = {
-    Future.successful(InternalServerError(toErrorStringToJson("Internal api error, try later")).as("application/vnd.api+json"))
+    Future.successful(InternalServerError(errorStringToJson("Internal api error, try later")).as("application/vnd.api+json"))
   }
 
   override def onHandlerNotFound(request: RequestHeader) = {
-    Future.successful(NotFound(toErrorStringToJson("Handler not found, check the API documentation")).as("application/vnd.api+json"))
+    Future.successful(NotFound(errorStringToJson("Handler not found, check the API documentation")).as("application/vnd.api+json"))
   }
 
 }
