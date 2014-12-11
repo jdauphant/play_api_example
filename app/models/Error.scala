d3c98b296eed7df77f4303a72b2eee76bc2b0f5f
Julien DAUPHANT <judauphant@gmail.com>
2014-12-11 17:32:26 +0100

    Simplify json format

--- app/models/Error.scala
+++ app/models/Error.scala
@@ -1,15 +1,9 @@
 package models
 
 import play.api.libs.json._
-import play.api.libs.functional.syntax._
-import utils.Hash
 
 case class Error(title: String, id: Option[String] = None, detail: Option[String] = None)
 
 object Error {
-  implicit val errorWrites: Writes[Error] = (
-      (JsPath \ "title").write[String] and
-      (JsPath \ "id").writeNullable[String] and
-      (JsPath \ "detail").writeNullable[String]
-    )(unlift(Error.unapply))
+   val errorFormat = Json.format[Error]
 }
--- app/models/TopLevel.scala
+++ app/models/TopLevel.scala
@@ -1,14 +1,10 @@
 package models
 
-import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
 case class TopLevel(links: Option[Map[String,String]] = None, errors: Option[Error] = None)
 
 object TopLevel {
-  implicit val errorWrites = Error.errorWrites
-  implicit val topLevelWrites: Writes[TopLevel] = (
-      (JsPath \ "links").writeNullable[Map[String,String]] and
-      (JsPath \ "errors").writeNullable[Error]
-    )(unlift(TopLevel.unapply))
+  implicit val errorFormat = Error.errorFormat
+  implicit val topLevelFormat = Json.format[TopLevel]
 }
