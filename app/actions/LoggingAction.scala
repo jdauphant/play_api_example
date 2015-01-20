dbddd9ab1f54d79551a1367ee2d5353bc0c9e0a5
Julien DAUPHANT <judauphant@gmail.com>
2015-01-20 11:51:34 +0100

    More debug information

--- app/actions/LoggingAction.scala
+++ app/actions/LoggingAction.scala
@@ -9,11 +9,13 @@ import scala.concurrent.Future
 case class LoggingAction[A](action: Action[A]) extends Action[A] {
   def apply(request: Request[A]): Future[Result] = {
     action(request).map{ result =>
-      lazy val message = "%s %s %s => %s".format(request.method,request.path, request.body.toString, result.header.status)
+      lazy val message = "%s %s?%s %s => %s".format(request.method,request.path,request.rawQueryString, request.body.toString, result.header.status)
       if(result.header.status >= 400)
         Logger.error(message)
       else if(Logger.isDebugEnabled)
         Logger.debug(message)
+      else
+        Logger.info("%s %s?%s => %s".format(request.method,request.path,request.rawQueryString, result.header.status))
       result
     }
   }
